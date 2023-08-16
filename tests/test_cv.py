01bab23f5b4218918d140cf6d7ab708a9dbd97bb
Daniel Watkins <daniel@daniel-watkins.co.uk>
2023-08-15 20:34:05 -0400

    refactor common checks to helper

--- tests/test_cv.py
+++ tests/test_cv.py
@@ -27,19 +27,27 @@ class FakeConfigModule:
 @mock.patch("mylar.cv.mylar.CVURL", FAKE_CV_ROOT)
 class TestPullDetails:
 
+    @staticmethod
+    def _get_validated_parts(m_get):
+        assert 1 == m_get.call_count
+        get_args, _get_kwargs = m_get.call_args
+
+        url_parts = urlparse(get_args[0])
+        assert url_parts.scheme == "http"
+        assert url_parts.netloc == "cv"
+
+        query_parts = _unwrapping_parse_qs(url_parts.query)
+        assert query_parts["api_key"] == FAKE_CV_API_KEY
+
+        return url_parts, query_parts
+
     def test_comic(self):
         with mock.patch("mylar.cv.requests.get") as m_get:
             m_get.return_value.content = b'<?xml version="1.0" encoding="utf-8"?><response/>'
             cv.pulldetails("1234", "comic")
 
-        assert 1 == m_get.call_count
-        get_args, _get_kwargs = m_get.call_args
-        url_parts = urlparse(get_args[0])
-        assert url_parts.scheme == "http"
-        assert url_parts.netloc == "cv"
+        url_parts, query_parts = self._get_validated_parts(m_get)
         assert url_parts.path == "/volume/4050-1234/"
 
-        query_parts = _unwrapping_parse_qs(url_parts.query)
-        assert query_parts["api_key"] == FAKE_CV_API_KEY
         assert query_parts["format"] == "xml"
         assert query_parts["field_list"] == "name,count_of_issues,issues,start_year,site_detail_url,image,publisher,description,first_issue,deck,aliases"
